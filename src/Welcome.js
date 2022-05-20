f63989b60d4cf8a140313037580c8f6993048c00
George Nepharidze <you@example.com>
2022-05-20 05:39:37 +0200

    finished ConditionalRendering-02

--- src/Welcome.js
+++ src/Welcome.js
@@ -6,14 +6,12 @@ export class Welcome extends React.Component {
     return (
       <div>
         <p>Welcome {this.props.name}!</p>
-        {this.props.age > 18 && <Age age={this.props.age} />}
+        {this.props.age && <Age age={this.props.age} />}
       </div>
     );
   }
 }
 
-
 Welcome.defaultProps = {
   name: "Giorgi",
-}
-
+};
