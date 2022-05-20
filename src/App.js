78694098356ce7d22c1dc1162e2a0ef07a567217
George Nepharidze <you@example.com>
2022-05-20 03:11:16 +0200

    finished props-01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { HellowWorld } from "./HellowWorld";
+import { Welcome } from "./Welcome";
 
 function App() {
   return (
     <div className="App">
       <HellowWorld />
+      <Welcome name="Giulia" />
     </div>
   );
 }
