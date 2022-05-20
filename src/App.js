6dfb2d844405e20ffd51635119bbb7e4638a6d38
George Nepharidze <you@example.com>
2022-05-20 02:17:23 +0200

    finished components-02

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import HellowWorld from './HellowWorld';
+
+function App() {
+  return (
+    <div className="App">
+      <HellowWorld />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/HellowWorld.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HellowWorld() {
+  return (
+    <div>
+       <h1>Hellow World!</h1>
+    </div>
+  )
+}
