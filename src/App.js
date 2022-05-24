9a8af3a05837fd9febc875477ffe94b48b1dca55
George Nepharidze <you@example.com>
2022-05-24 21:19:41 +0200

    finished fomrs-06

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ClickCounter from "./ClickCounter";
-import Counter from "./Counter";
-import { HellowWorld } from "./HellowWorld";
-import { Welcome } from "./Welcome";
-import ClickTracker from "./ClickTracker";
-import { InteractiveWelcome } from "./InteractiveWelcome";
-import { Login } from "./Login";
+// import ClickCounter from "./ClickCounter";
+// import Counter from "./Counter";
+// import { HellowWorld } from "./HellowWorld";
+// import { Welcome } from "./Welcome";
+// import ClickTracker from "./ClickTracker";
+// import { InteractiveWelcome } from "./InteractiveWelcome";
+// import { Login } from "./Login";
 import UncontrolledLogin from "./UncontrolledLogin ";
 
 export class App extends React.Component {
--- src/UncontrolledLogin .js
+++ src/UncontrolledLogin .js
@@ -8,11 +8,16 @@ export default class UncontrolledLogin extends Component {
     this.props.onLogin(username, password, checkbox);
     console.log(username, password, checkbox);
   };
+
+  componentDidMount = () => {
+    this.username.focus();
+  };
+
   render() {
     return (
       <div>
         <form onSubmit={this.submitForm}>
-          <input name="username"></input>
+          <input ref={input => (this.username = input)} name="username" type="text"></input>
           <input name="password" type="password"></input>
           <input name="checkbox" type="checkbox"></input>
           <button name="submit-btn" type="submit">
