bfd8b79d250d5b88f7d892fdfc8967c972442b42
George Nepharidze <you@example.com>
2022-05-27 22:22:27 +0200

    finished styling-components-01

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React from "react";
-import ToDoList from "./ToDoList";
+// import ToDoList from "./ToDoList";
 // import ClickCounter from "./ClickCounter";
 // import Counter from "./Counter";
 // import { HellowWorld } from "./HellowWorld";
-// import { Welcome } from "./Welcome";
+import { Welcome } from "./Welcome";
 // import ClickTracker from "./ClickTracker";
 // import { InteractiveWelcome } from "./InteractiveWelcome";
 // import { Login } from "./Login";
-import UncontrolledLogin from "./UncontrolledLogin ";
+// import UncontrolledLogin from "./UncontrolledLogin ";
+import "./index.css";
 
 export class App extends React.Component {
   render() {
     return (
       <div>
-        {/* <HellowWorld />
+        {/* <HellowWorld /> */}
         <Welcome name="John" age={17} />
         <Welcome name="John" age={37} />
-        <Counter initial ={0}  incrementBy={1}  incrementEvery={1000}/>
+        {/* <Counter initial ={0}  incrementBy={1}  incrementEvery={1000}/>
         <ClickCounter />
         <ClickTracker />
         <InteractiveWelcome /> */}
         {/* <Login /> */}
         {/* <UncontrolledLogin /> */}
-        <ToDoList />
+        {/* <ToDoList /> */}
       </div>
     );
   }
