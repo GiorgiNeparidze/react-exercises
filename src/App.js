171bff61ec75d534c850f3a351b4060f940829db
George Nepharidze <you@example.com>
2022-06-02 01:32:30 +0200

    resolved merge conflict

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from "react";
-<<<<<<< HEAD
 
-// import ClickCounter from "./ClickCounter";
-=======
 // import Counter from "./Counter";
 // import Counter from "./Counter";
 import ClickCounter from "./ClickCounter";
->>>>>>> useEffect-01
 // import Counter from "./Counter";
 // import { HellowWorld } from "./HellowWorld";
 // import { Welcome } from "./Welcome";
 // import ClickTracker from "./ClickTracker";
 // import { InteractiveWelcome } from "./InteractiveWelcome";
-import  Login  from "./Login";
+// import  Login  from "./Login";
 
 export class App extends React.Component {
-
   render() {
     return (
       <div>
@@ -23,15 +18,12 @@ export class App extends React.Component {
         <Welcome name="John" age={17} />
         <Welcome name="John" age={37} />
         <Counter initial ={0}  incrementBy={1}  incrementEvery={1000}/> */}
-<<<<<<< HEAD
         {/* <ClickCounter />
         <ClickTracker />
         <InteractiveWelcome />  */}
-        <Login />
-=======
+        {/* <Login /> */}
         <ClickCounter onCounterChange={(counter) => console.log(counter)} />
         {/* <Counter /> */}
->>>>>>> useEffect-01
       </div>
     );
   }
