9db753cf4c4640b5a31d378c3365d3a52c1cfe75
George Nepharidze <you@example.com>
2022-06-02 14:05:39 +0200

    finished useCallback

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import GithubUser from "./GithubUser";
-import GithubUserList from "./GithubUserList";
+// import GithubUser from "./GithubUser";
+// import GithubUserList from "./GithubUserList";
 // import Counter from "./Counter";
 // import Counter from "./Counter";
 // import ClickCounter from "./ClickCounter";
@@ -10,24 +10,17 @@ import GithubUserList from "./GithubUserList";
 // import ClickTracker from "./ClickTracker";
 // import { InteractiveWelcome } from "./InteractiveWelcome";
 // import  Login  from "./Login";
+import { useCounter } from "./useCounter";
 
-export class App extends React.Component {
-  render() {
-    return (
-      <div>
-        {/* <HellowWorld />
-        <Welcome name="John" age={17} />
-        <Welcome name="John" age={37} />
-        <Counter initial ={0}  incrementBy={1}  incrementEvery={1000}/> */}
-        {/* <ClickCounter /> */}
-        {/* <ClickTracker />
-        <InteractiveWelcome />  */}
-        {/* <Login /> */}
-        {/* <ClickCounter onCounterChange={(counter) => console.log(counter)} /> */}
-        {/* <Counter /> */}
-        <GithubUser username={'GiorgiNeparidze'}/>
-        <GithubUserList/>
-      </div>
-    );
-  }
+export function App({ initialvalue = 0 }) {
+  const { counter, increment, decrement, reset } = useCounter(initialvalue);
+
+  return (
+    <div>
+      <h1>You have clicked counter {counter} times!</h1>
+      <button onClick={increment}>Increment!</button>
+      <button onClick={decrement}>Decrement!</button>
+      <button onClick={reset}>Reset!</button>
+    </div>
+  );
 }
