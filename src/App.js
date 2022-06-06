356205bd5fa489525be18241cdf49ae92661ae26
George Nepharidze <you@example.com>
2022-06-06 18:34:02 +0200

    finished merged swr-01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import Counter from "./Counter";
 import "./index.css";
-import { Routes, Route, Link } from "react-router-dom";
-import ShowGithubUser from "./ShowGithubUser";
-import GithubUserList from "./GithubUserList";
 import GithubUser from "./GithubUser";
 
 export class App extends React.Component {
