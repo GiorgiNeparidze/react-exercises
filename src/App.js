cf50832e82db8be9083ebd23ac8fa288a81923ad
George Nepharidze <you@example.com>
2022-06-06 16:45:04 +0200

    finished react-router-04

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
-
+import Counter from "./Counter";
 import Welcome from "./Welcome";
 import "./index.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import ShowGithubUser from "./ShowGithubUser";
-
 
 export class App extends React.Component {
   render() {
     return (
-      <Routes>
-
-        <Route path='/' element={<Welcome name="John" />} />
-        <Route path="users/:username" element={<ShowGithubUser />}/>
-      </Routes>
+      <div>
+        <Routes>
+          <Route path="/" element={<Welcome name="John" />} />
+          <Route path="counter" element={<Counter />} />
+          <Route path="users/username" element={<ShowGithubUser />} />
+        </Routes>
+        <Link to={"/"}>Welcome !</Link>
+        <Link to={"/counter"}>Count !</Link>
+        <Link to={"users/username"}>Github User IS</Link>
+      </div>
     );
   }
 }
--- src/ShowGithubUser.js
+++ src/ShowGithubUser.js
@@ -3,7 +3,7 @@ import GithubUser from "./GithubUser";
 import { useParams } from "react-router-dom";
 
 export default function ShowGithubUser() {
-  const {username} = useParams();
+  const {username = "GiorgiNeparidze" } = useParams();
 
   return (
     <div>
